2abd5b38ef76e7343e56e3835bd3d44ca4411292
fruit-memory <315764194@qq.com>
2018-06-08 13:10:58 +0800

    增加请求重试的功能

--- Front end/fetch.js
+++ Front end/fetch.js
@@ -4,9 +4,15 @@ class TimeoutError extends Error {
     this.name = 'TimeoutError';
   }
 }
-
+/**
+ * 提供参数校验和wrapper功能
+ *
+ * @param {Object or String} defaultOptions
+ * @returns {Promise} the request result
+ */
 function request(defaultOptions) {
   let options = defaultOptions;
+  let retryCount = 0;
   if (typeof options === 'string') {
     options = {
       url: defaultOptions,
@@ -30,28 +36,35 @@ function request(defaultOptions) {
   };
 
   class Fetch {
-    constructor({ timeout, ...options }) {
-      this.url = options.url;
+    constructor({ timeout, url, retry, ...options }) {
+      this.url = url;
+      this.retry = retry || 0;
       this.timeout = timeout || 10000;
       this.options = options;
     }
 
     then(fn) {
-      let success = false;
+      let done = false;
       setTimeout(() => {
-        if (!success) {
+        // 无论是请求重试还是最终超时错误，此次请求得到的结果作废
+        if (retryCount < this.retry && !done) {
+          done = true;
+          retryCount++;
+          this.then(fn);
+        } else {
           let error = new TimeoutError(`fetch timeout (${this.timeout / 1000}s)`);
           this.catchError(error);
         }
       }, this.timeout);
-      
+
       fetch(this.url, this.options)
         .then(checkStatus)
         .then(parseJSON)
         .then(res => {
-          if (!success) {
+          // 未进入重试或者超时错误，返回结果
+          if (!done) {
             fn(res);
-            success = true;
+            done = true;
           }
         })
         .catch(err => {
@@ -83,16 +96,13 @@ request({
   headers: {
     'Content-Type': 'application/json'
   },
-  timeout: 66
+  retry: 2,
+  timeout: 660
 })
-  .then(res => {
-    console.log(res);
-  })
-  .catch(err => {
-    console.log(err);
-  });
+  .then(res => console.log(res))
+  .catch(err => console.log(err));
 
 Promise.all([
   request('https://cnodejs.org/api/v1/topic/5433d5e4e737cbe96dcef312'),
   request('https://cnodejs.org/api/v1/topic/5433d5e4e737cbe96dcef312')
-]);
+]).then(res => console.log(res));
