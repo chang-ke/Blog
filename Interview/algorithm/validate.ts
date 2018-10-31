13fe63e7d57f1f1319efb4862973dd51f36bcc05
fruit-memory <315764194@qq.com>
2018-10-31 15:07:55 +0800

    fix validate

--- Interview/algorithm/validate.ts
+++ Interview/algorithm/validate.ts
@@ -5,7 +5,7 @@
  * @returns
  */
 function validateCircularRefrence(target: Object) {
-  const q = [];
+  const q: Array<any> = [];
   /**保存访问当前节点的路径，并判断当前节点是否与路径上的节点是否属于同一个实例 */
   return (function dfs(origin: any) {
     if (!origin) return;
